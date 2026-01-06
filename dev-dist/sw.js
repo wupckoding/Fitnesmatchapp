dbb5fc6d4c7aa8804ed97dacb5c743d53d983872
Bruno De Sousa Henz <wupckoding@gmail.com>
2026-01-06 18:44:47 -0300

    100% tem que estar esse

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.f8dki19ealk"
+    "revision": "0.i0s99r8rel"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
